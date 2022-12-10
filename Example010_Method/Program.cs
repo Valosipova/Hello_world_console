8c0e7d6039b0454a3d0c34fcdc8fd4e1c3b4d326
Valentina <valush31@gmail.com>
2022-12-10 21:10:03 +0300

    add new example 010

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {11, 22, 13, 41, 8, 15, 61, 17, 8};
+
+int n = array.Length;
+
+int find = 61;
+
+int index = 0;
+
+while (index < n)
+
+{
+    
+    if (array [index] == find)
+    {
+       Console.WriteLine(index); 
+       break;
+    }
+
+    //index=index+1;
+    index++;
+}
